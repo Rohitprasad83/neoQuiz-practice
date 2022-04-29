24e67e401cc27e3f444d01d96c9bedfa543858d5
Rohitprasad83 <pdrohit674@gmail.com>
2022-04-29 23:47:26 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // run `node index.js` in the terminal
+const person = {
+  fullName: function (city, country) {
+    console.log(
+      this.firstName + ' ' + this.lastName + ',' + city + ',' + country
+    );
+  },
+};
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const person2 = {
+  firstName: 'John',
+  lastName: 'Dave',
+};
+
+person.fullName.call(person2, ['Oslo', 'Norway']);
